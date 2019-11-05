7a5b1a5a4ed723847e83cf2415b19089b41da78e
Levi Sandoval (Mac) <l.sandoval@mefasa.org>
2019-11-05 16:04:54 -0700

    mas cambios para Gain, que ya esta comprobado con el ejemplo

--- decision_tree/InfoGain.py
+++ decision_tree/InfoGain.py
@@ -20,13 +20,12 @@ def Gain(S, A):
 
   for s in S:
     value = s[A]
-    if value in values:
-      if s[masterkey] == "yes":
-        values[value][0] = values[value][0] + 1
-      elif s[masterkey] == "no":
-        values[value][1] = values[value][1] + 1
-    else:
+    if value not in values:
       values[value] = [0, 0]
+    if s[masterkey] == "yes":
+      values[value][0] = values[value][0] + 1
+    elif s[masterkey] == "no":
+      values[value][1] = values[value][1] + 1
 
   #return S_entropy
   return values
