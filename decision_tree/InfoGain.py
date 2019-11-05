2cf76535cf23756400e138fcfd0e2935e4629af5
levinux <levisan@gmail.com>
2019-11-04 21:33:38 -0700

    Se agrego la funcion countPN para la entropia de todos los ejemplos, se modifico main para ejecutar Gain que este a su vez saca la entropia global, y habia un error en los ejemplos de entrenamiento

--- decision_tree/InfoGain.py
+++ decision_tree/InfoGain.py
@@ -1,5 +1,19 @@
 from Entropy import binEntropy as entropy
 
-def Gain():
-  pass
+masterkey = "playtennis"
+
+def countPN(training, key, pv="yes", nv="no"):
+  pos = 0
+  neg = 0
+
+  for example in training:
+    if example[key] == pv: pos = pos + 1
+    elif example[key] == nv: neg = neg + 1
+
+  return pos, neg
+
+def Gain(S, A):
+  Spos, Sneg = countPN(S, masterkey)
+  S_entropy = entropy(Spos, Sneg)
+  return S_entropy
 
--- decision_tree/main.py
+++ decision_tree/main.py
@@ -3,6 +3,6 @@ from BuildFromTraining import loadFromFile
 
 print("Cargando ejemplos de entrenamiento...")
 t_examples = loadFromFile("training_examples.txt")
-#print(t_examples)
+print(Gain(t_examples, []))
 print("Calculando ganancia de informacion...")
 
