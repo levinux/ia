6452c2a9ff1896e19583879950d678a635ad2909
levinux <levisan@gmail.com>
2019-11-07 23:11:48 -0700

    Lista la funcion de Gain (Entropia de columnas)

--- decision_tree/InfoGain.py
+++ decision_tree/InfoGain.py
@@ -15,17 +15,27 @@ def countPN(training, key, pv="yes", nv="no"):
 def Gain(S, A):
   gain = 0
   values = {}
+  lenS = len(S)
   Spos, Sneg = countPN(S, masterkey)
   S_entropy = entropy(Spos, Sneg)
 
   for s in S:
     value = s[A]
     if value not in values:
-      values[value] = [0, 0]
+      values[value] = [0, 0, 0]
     if s[masterkey] == "yes":
       values[value][0] = values[value][0] + 1
     elif s[masterkey] == "no":
       values[value][1] = values[value][1] + 1
 
-  #return S_entropy
-  return values
+  acum = 0
+  S_At = 0
+  propt = 0
+  for k, v in values.items():
+    v[2] = entropy(v[0], v[1])
+    propt = (v[0] + v[1]) / lenS
+    acum= propt * v[2]
+    S_At = S_At - acum
+
+  return S_entropy + S_At
+
