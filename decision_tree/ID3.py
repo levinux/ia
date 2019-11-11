61a78ab219b2b4c519beff688b064cea4d2fbcf4
levinux <levisan@gmail.com>
2019-11-11 00:28:27 -0700

    Ya se empezo con el esqueleto de ID3

--- decision_tree/ID3.py
+++ decision_tree/ID3.py
@@ -1,1 +1,17 @@
 # Archivo vacio por el momento MUAHAHAHAHA
+
+def ExamplesArePos(Examples):
+  pass
+
+def ExamplesAreNeg(Examples):
+  pass
+
+def id3(Examples, TargetA, Attributes):
+  Root = ""
+  if ExamplesArePos(Examples): return (Root, "+")
+  if ExamplesAreNeg(Examples): return (Root, "-")
+  if len(Attributes) == 0: return (Root, "=")
+  ###
+
+  ### Otherwise ###
+  pass
