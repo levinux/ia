d77d32fdeebdd651a2be3fcbbd982041a9929f66
Levi Sandoval (Mac) <l.sandoval@mefasa.org>
2019-11-29 22:25:36 -0700

    ya esta hecho a grandes rasgos el algoritmo... falta armar el arbol y probar la recursividad

--- decision_tree/ID3.py
+++ decision_tree/ID3.py
@@ -20,6 +20,14 @@ def BestGainAttribute(S, Attrs):
   Attrs.remove(bestA)
   return (bestA, bestV)
 
+def valuesOfA(Examples, Attribute):
+  valuesInA = []
+  for example in Examples:
+    valueOfA = example[Attribute]
+    if valueOfA not in valuesInA:
+      valuesInA.append(valueOfA)
+  return valuesInA
+
 def id3(Examples, TargetA, Attributes):
   A = {}
   Root = ""
@@ -31,5 +39,10 @@ def id3(Examples, TargetA, Attributes):
   ### Otherwise ###
   A = BestGainAttribute(Examples, Attributes)
   arbol[A[0]] = {}
-  print(arbol)
-  print(Attributes)
+  valuesInA = valuesOfA(Examples, A[0])
+  examplesV = []
+  for value in valuesInA:
+    for i in range(0, len(Examples)):
+      if Examples[i][A[0]] == value:
+        examplesV.append(Examples[i])
+    id3(examplesV, TargetA, Attributes)
